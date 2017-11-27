e8f1265c7354bae6274831cf059a7867bf977530
Nix Li <xinli1997@gmail.com>
2017-11-27 17:38:37 -0500

    Update server.c

--- server.c
+++ server.c
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   main.c
- * Author: lixin39
+ * Author: lixin
  *
  * Created on November 6, 2017, 3:31 PM
  */
